dc5e20494f2b41382cfb5c6b8c8b14b81f3f779b
samuelayano <samuelayano7@gmail.com>
2023-02-04 13:29:18 +0100

    created testimonial section

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,14 @@
-ECHO is on.
+// initialize Swiper js
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    grabCursor:true,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
