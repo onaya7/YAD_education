aac86eb73530c170268d33ee0111fe35c7d50bec
samuelayano <samuelayano7@gmail.com>
2023-02-05 06:47:15 +0100

    added media query to css

--- static/js/script.js
+++ static/js/script.js
@@ -12,3 +12,17 @@ var swiper = new Swiper(".mySwiper", {
       prevEl: ".swiper-button-prev",
     },
   });
+
+
+let menu = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle("active");
+};
+
+window.onscroll = () => { 
+    menu.classList.remove('bx-x');
+    navbar.classList.remove("active")
+};
