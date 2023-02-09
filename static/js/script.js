4ba31750cf62d9ccd074c10a977084ea31040f0a
samuelayano <samuelayano7@gmail.com>
2023-02-09 23:48:48 +0100

    updated university pages 2

--- static/js/script.js
+++ static/js/script.js
@@ -13,7 +13,7 @@ var swiper = new Swiper(".mySwiper", {
     },
   });
 
-
+// Mobile responsive nav
 let menu = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
@@ -26,3 +26,22 @@ window.onscroll = () => {
     menu.classList.remove('bx-x');
     navbar.classList.remove("active")
 };
+
+
+// search filter for universitys
+function search() {
+  let filter = document.getElementById('find').value.toUpperCase();
+  let item = document.querySelectorAll('.card');
+  let l = document.getElementsByTagName('h3');
+  for(var i = 0;i<=l.length;i++){
+  let a=item[i].getElementsByTagName('h3')[0];
+  let value=a.innerHTML || a.innerText || a.textContent;
+  if(value.toUpperCase().indexOf(filter) > -1) {
+  item[i].style.display="";
+  }
+  else
+  {
+  item[i].style.display="none";
+  }
+  }
+  }
